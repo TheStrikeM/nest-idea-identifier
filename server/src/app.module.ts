8b9bbdbf2d55b4cec8b0c08ce4f54943077a64e2
TheStrikeM <thestrikem@gmail.com>
2021-08-08 03:51:14 +0500

    Add typeorm and start dev idea module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,21 @@
 import {Module} from "@nestjs/common";
+import { IdeaModule } from './idea/idea.module';
 import LoggerModule from "./logger/logger.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+import TypeormConfig from "./utils/typeorm.config";
 
 @Module({
-    imports: [LoggerModule]
+    imports: [
+        LoggerModule,
+        IdeaModule,
+        TypeOrmModule.forRootAsync({
+            useClass: TypeormConfig,
+        }),
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+        }),
+    ]
 })
 export default class AppModule {}
